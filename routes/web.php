d6b2b0be2ee8cf7c8f340fda560aedc4aed964af
Gordon39 <gordonsim88@gmail.com>
2025-03-26 14:32:57 +0800

    Route updated

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,18 @@ Route::get('/', function () {
 });
 
 
-Route::get('/homepage', function () {
-        return view('homepage');
-    })->name('homepage');
+Route::get('/userhomepage', function () {
+        return view('userhomepage');
+    })->name('userhomepage');
 
 Route::get('/register', function () {
     return view('register');
 })->name('register');
+
+Route::get('/vendorhomepage', function () {
+    return view('vendorhomepage');
+})->name('vendorhomepage');
+
+Route::get('/riderhomepage', function () {
+    return view('riderhomepage');
+})->name('riderhomepage');
